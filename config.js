4c443abf1b756f807f8473b942a012889492da59
Payam Azadi <payamazadi@Payams-Mac-mini.fios-router.home>
2020-11-08 23:47:02 -0500

    config

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://www.azadismind.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
+  title: 'Engineering Management, by Payam Azadi',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: '',
   useKatex: false,
   menu: [
     {
@@ -25,9 +25,9 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Payam Azadi',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'This is my bio',
     contacts: {
       email: '',
       facebook: '#',
